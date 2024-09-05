3428fc4f1cc79f8170cb42c42be165e77d2e4378
Taha A. Tas <tas_ta@gsc218-node-9.mercer.edu>
2024-09-05 10:45:19 -0400

    initial glInit, myInit, and main created

---
+++ poly.c
@@ -0,0 +1,37 @@
+#include <GL/glut.h>
+#include <stdio.h>
+
+const int WINDOW_POSITION_X = 100;
+const int WINDOW_POSITION_Y = 100;
+const int WINDOW_MAX_X = 400;
+const int WINDOW_MAX_Y = 400;
+
+void myglutInit(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(WINDOW_MAX_X, WINDOW_MAX_Y);
+	glutInitWindowPosition(WINDOW_POSITION_X, WINDOW_POSITION_Y);
+	glutCreateWindow("Polygon Tesselation");
+}
+
+void myInit(void)
+{
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//Insert gluOrtho2D
+	glMatrixMode(GL_MODELVIEW);
+
+}
+
+int main(int argc. char** argv)
+{
+	myglutInit(argc,argv);
+	myInit();
+
+	glutMouseFunc(mouse);
+	glutKeyboardFunc(keyboard);
+	glutDisplayFunc(mouse);
+	glutMainLoop();
+}
